1980b9620babb5e9e97598184cd443fece03d330
Divya <beingdivyadixit@gmail.com>
2023-01-24 16:35:34 +0530

    Updatated to difrent adress book by using dic UC6

--- AddressBookSystem/Person.cs
+++ AddressBookSystem/Person.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -10,120 +12,130 @@ using System.Xml.Linq;
 namespace AddressBookSystem
 {
     public class Person // class 
-    {       
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-            public string Address { get; set; }
-            public string City { get; set; }
-            public string ZipCode { get; set; }
-            public string PhoneNumber { get; set; }
-            public string Email { get; set; }
-            public static List<Person> people = new List<Person>(); // list
-
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string zipCode { get; set; }
+        public string phoneNumber { get; set; }
+        public string email { get; set; }
+        public string addressBookName { get; set; }
+        public static List<Person> people; // list
+                                                                //public static Dictionary<string, List<Person>> contactDictionary = new Dictionary<string, List<Person>>();
+        public static Dictionary<string, List<Person>> contactDictionary;
         // default constructor
         public Person()
         {
 
         }
-        // constructor
-        public Person(string FirstName, string LastName, string Address, string City, string ZipCode, string PhoneNumber, string Email)
-        {
-            this.FirstName = FirstName;
-            this.LastName = LastName;
-            this.Address = Address;
-            this.City = City;
-            this.ZipCode = ZipCode;
-            this.PhoneNumber = PhoneNumber;
-            this.Email = Email;
-            
+        // constructor with parameter
+        public Person(string adressBookName, string firstName, string lastName, string address, string city, string zipCode, string phoneNumber, string email)
+        { 
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.zipCode = zipCode;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+            this.addressBookName = addressBookName;
+            people = new List<Person>();
+            contactDictionary = new Dictionary<string, List<Person>>();
         }
         // Method for input contact details  
-        public void EntryDetails(int numContact)
+        public void EntryDetails()
         {
-            for (int i = 0; i < numContact; i++)
+            Console.WriteLine("Enter the Number of Contact You Wanted to make");
+            int numContact = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= numContact; i++)
             {
-                Person person = new Person(); ;
+                Person person = new Person();
+                Console.WriteLine("Enter the details of Contact {0}",i); ;
                 Console.WriteLine("Enter your 1st Name:");
-                person.FirstName = Console.ReadLine();
+                person.firstName = Console.ReadLine();
                 Console.WriteLine("Enter your last Name:");
-                person.LastName = Console.ReadLine();
+                person.lastName = Console.ReadLine();
                 Console.WriteLine("Enter your Address:");
-                person.Address = Console.ReadLine();
+                person.address = Console.ReadLine();
                 Console.WriteLine("Enter your City:");
-                person.City = Console.ReadLine();
+                person.city = Console.ReadLine();
                 Console.WriteLine("Enter your Zip Code:");
-                person.ZipCode = Console.ReadLine();
+                person.zipCode = Console.ReadLine();
                 Console.WriteLine("Enter your Phone Number:");
-                person.PhoneNumber = Console.ReadLine();
+                person.phoneNumber = Console.ReadLine();
                 Console.WriteLine("Enter your Email:");
-                person.Email = Console.ReadLine();
+                person.email = Console.ReadLine();
                 people.Add(person);
             }
         }
         // method for display contact details of adress book
-        public static void DisplayAdressBook()
+        public void DisplayAdressBook()
         {
-           foreach (Person person in people )
-           {
-                Console.WriteLine("Here is the adress book of person");
-                Console.WriteLine("First Name:" + person.FirstName);
-                Console.WriteLine("Last Name:" + person.LastName);
-                Console.WriteLine("Adress:" + person.Address);
-                Console.WriteLine("City:" + person.City);
-                Console.WriteLine("Zip Code:" + person.ZipCode);
-                Console.WriteLine("Phone Number:" + person.PhoneNumber);
-                Console.WriteLine("Email:" +person.Email);
+            //Person person = new Person(addressBookName,firstName, lastName, address, city, zipCode, phoneNumber, email);
+            foreach (var person in people)
+            {
+                Console.WriteLine("\n" + person.addressBookName);
+                Console.WriteLine("First Name:" + person.firstName) ;
+                Console.WriteLine("Last Name:" + person.lastName);
+                Console.WriteLine("Adress:" + person.address);
+                Console.WriteLine("City:" + person.city);
+                Console.WriteLine("Zip Code:" + person.zipCode);
+                Console.WriteLine("Phone Number:" + person.phoneNumber);
+                Console.WriteLine("Email:" + person.email);
                 Console.WriteLine("***************************************************");
-           }
+            }
 
         }
         // Method for editing contact details
-        public void EditContact(string name)
+        public void EditContact()
         {
+            Console.WriteLine("Enter the first name you want to edit information");
+            string name = Convert.ToString(Console.ReadLine());
             bool input = false;
             Console.WriteLine("Enter: \n 1 for edit first name \n 2 for edit last name \n 3 for edit adress \n 4 for edit city \n 5 for edit zip code \n 6 for edit phone number \n 7 for edit email ");
             int option = Convert.ToInt32(Console.ReadLine());
-            foreach (Person person in people)
+            foreach (var person in people)
             {
-                if (person.FirstName == name) // if statement
+                if (person.firstName == name) // if statement
                 {
                     input = true;
-                    switch (option) 
+                    switch (option)
                     {
                         case 1:
                             Console.WriteLine("Enter Curent first name :");
                             string currentFirstName = Convert.ToString(Console.ReadLine());
-                            person.FirstName = currentFirstName;
+                            person.firstName = currentFirstName;
                             break;
                         case 2:
                             Console.WriteLine("Enter Curent last name :");
                             string currentLastName = Convert.ToString(Console.ReadLine());
-                            person.LastName = currentLastName;
+                            person.lastName = currentLastName;
                             break;
                         case 3:
                             Console.WriteLine("Enter Curent City Address :");
                             string currentAddress = Convert.ToString(Console.ReadLine());
-                            person.Address = currentAddress;
+                            person.address = currentAddress;
                             break;
                         case 4:
                             Console.WriteLine("Enter Curent City :");
                             string currentCity = Convert.ToString(Console.ReadLine());
-                            person.City = currentCity;
+                            person.city = currentCity;
                             break;
                         case 5:
                             Console.WriteLine("Enter Curent Zip Code :");
                             string currentZipCode = Convert.ToString(Console.ReadLine());
-                            person.ZipCode = currentZipCode;
+                            person.zipCode = currentZipCode;
                             break;
                         case 6:
                             Console.WriteLine("Enter Curent Phone Number :");
                             string currentPhoneNumber = Convert.ToString(Console.ReadLine());
-                            person.PhoneNumber = currentPhoneNumber;
+                            person.phoneNumber = currentPhoneNumber;
                             break;
                         case 7:
                             Console.WriteLine("Enter Curent Email :");
                             string currentEmail = Convert.ToString(Console.ReadLine());
-                            person.Email = currentEmail;
+                            person.email = currentEmail;
                             break;
                         default:
                             Console.WriteLine("Please Enter valid Option");
@@ -131,27 +143,46 @@ namespace AddressBookSystem
                     }
 
                 }
-               
+
             }
-            if(input==false)
-            Console.WriteLine("First name not found");
-            Person.DisplayAdressBook();  // calling method for displaying after edit
+            if (input == false)
+                Console.WriteLine("First name not found");
+            
         }
-        public void deleteContact(string name)
-        { bool input = false;
-          foreach (Person person1 in people)
-          {
-               if (person1.FirstName == name) // if statement
-               {
+        public void deleteContact()
+        {   bool input = false;
+            Console.WriteLine("enter the first name of contact you want to delete");
+            string name = Convert.ToString(Console.ReadLine());
+            foreach (var person1 in people)
+            {
+                if (person1.firstName == name) // if statement
+                {
                     input = true;
                     people.Remove(person1); // for delting 
                     Console.WriteLine("adress book of person after remove");
                     break;
-               }
-          }
+                }
+            }
             if (input == false) // if statement
                 Console.WriteLine("Contact not present");
         }
+        public void AddNewAddressBook()
+        {
+            //Creating a dictionary
+            //Dictionary<string, List<Person>> contactDictionary = new Dictionary<string, List<Person>>();
+            Person person = new Person(addressBookName, firstName,lastName,address,city,zipCode,phoneNumber,email);
+            Console.WriteLine("How many number of address books you want to add ");
+            int numberOfBooks = Convert.ToInt32(Console.ReadLine());
+            while (numberOfBooks > 0)
+            {
+                Console.WriteLine("Enter name of the address book:");
+                person.addressBookName = Console.ReadLine();
+                EntryDetails();
+                contactDictionary.Add(person.addressBookName,people);  // to store book name and contacts in dictionary
+                Console.WriteLine("\n" + person.addressBookName); //to print the address-book Name
+                DisplayAdressBook(); //to display contacts
+                numberOfBooks--;
+            }
+        }
     }
-
 }
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Reflection.Emit;
 using System.Security.Cryptography.X509Certificates;
 using System.Transactions;
+using System.Xml.Linq;
 
 namespace AddressBookSystem
 {
@@ -12,63 +13,56 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {   // start
             Console.WriteLine("Welcome to AddressBook Program");
-            Console.WriteLine("Enter: \n1: Ability to create a Contacts in AdressBook\n2: Ability to add a new Contact to Address Book\n3: Edit Contact From Adress Book\n4: Delete contact \n5: Multiple contact"); // taken options of program input from user
+            Console.WriteLine("Enter: \n1: Ability to create a Contacts in AdressBook\n2: Ability to add a new Contact to Address Book\n3: Edit Contact From Adress Book\n4: Delete contact \n5: Multiple contact \n6: Refactor by using Dictionory"); // taken options of program input from user
             int option =Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
                 case 1:
                     {   //UC1
-                        Console.WriteLine("Enter the Number of Contact You Wanted to make");
-                        int numContact = Convert.ToInt32(Console.ReadLine());
-                        Person entryDetails = new Person();
-                        entryDetails.EntryDetails(numContact);
+                        Person person = new Person();
+                        person.EntryDetails();
                     }
                     break;
                 case 2:
                     {    //UC2
-                        Console.WriteLine("Enter the Number of Contact You Wanted to make");
-                        int numContact = Convert.ToInt32(Console.ReadLine());
-                        Person entryDetails = new Person();
-                        entryDetails.EntryDetails(numContact);
-                        Person.DisplayAdressBook();
+                        Person person = new Person();
+                        person.EntryDetails();
+                        person.DisplayAdressBook();
                     }
                     break;
                 case 3:
                     {   //UC3
-                        Console.WriteLine("Enter the Number of Contact You Wanted to make");
-                        int numContact = Convert.ToInt32(Console.ReadLine());
-                        Person entryDetails = new Person();
-                        entryDetails.EntryDetails(numContact);
-                        Person.DisplayAdressBook();
-                        Console.WriteLine("Enter the first name you want to edit information");
-                        string name = Convert.ToString(Console.ReadLine());
-                        entryDetails.EditContact(name);
+                        Person person = new Person();
+                        person.EntryDetails();
+                        person.DisplayAdressBook();
+                        person.EditContact();
+                        person.DisplayAdressBook();
                     }
                     break;
                 case 4:
                     {  //uc4
-                        Console.WriteLine("Enter the Number of Contact You Wanted to make");
-                        int numContact = Convert.ToInt32(Console.ReadLine());
-                        Person obj = new Person();
-                        obj.EntryDetails(numContact);
-                        Person.DisplayAdressBook();
-                        Console.WriteLine("enter the first name of contact you want to delete");
-                        string name=Convert.ToString(Console.ReadLine());
-                        obj.deleteContact(name);
-                        Person.DisplayAdressBook();
+                        Person person = new Person();
+                        person.EntryDetails();
+                        person.DisplayAdressBook();
+                        person.deleteContact();
+                        person.DisplayAdressBook();
 
                     }
                     break;
                 case 5:
                     {  // UC5
-                        Console.WriteLine("Enter the Number of Contact You Wanted to make");
-                        int numContact = Convert.ToInt32(Console.ReadLine());
-                        Person obj = new Person();
-                        obj.EntryDetails(numContact);
-                        Person.DisplayAdressBook();
+                        Person person = new Person();
+                        person.EntryDetails();
+                        person.DisplayAdressBook();
                     }
                     break;
-
+                case 6:
+                    {
+                        // UC6
+                        Person person = new Person();
+                        person.AddNewAddressBook();
+                    }
+                    break;
                 default:
                     Console.WriteLine("Please Enter The Valid Option");
                     break;
